99c6ab683e73203a87c93f7abbcb52b65224f098
chrissiku <chrissiku5@gmail.com>
2022-09-04 14:11:11 +0200

    Implement `owlCarousel` wih jquery

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".nav-bar").addClass("sticky-top");
+    } else {
+      $(".nav-bar").removeClass("sticky-top");
+    }
+  });
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
+})(jQuery);
