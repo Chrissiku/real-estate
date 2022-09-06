949951cb50b3586bc3406a4bf620acbf23b4a231
chrissiku <chrissiku5@gmail.com>
2022-09-06 13:23:37 +0200

    Add js function for carousel part

--- js/main.js
+++ js/main.js
@@ -22,6 +22,28 @@
     ],
   });
 
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 24,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      992: {
+        items: 2,
+      },
+    },
+  });
+
   // Back to top Link
   $(window).scroll(function () {
     if ($(this).scrollTop() > 500) {
